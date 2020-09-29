006fd136e74e2daf5dfea993a61601606b4b29e6
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-09-29 16:13:40 -0500

    created for loop

--- client.js
+++ client.js
@@ -41,3 +41,47 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+
+// ### Employee Array
+// Each **Employee** currently is configured in this way:
+
+// * The `name` property holds the employees name.
+// * The `employeeNumber` property has their employee number.
+// * The `annualSalary` property contains their base annual salary.
+// * The `reviewRating` property contains their review rating.
+
+// ## Processing Employee Bonuses
+
+// Loop over the `employees` array and do the following:
+
+for( i = 0; i < employees.length; i++ ){
+  console.log( 'In for loop', employees[i] );
+  function loopEmployees( employees ){
+    let newObj = [];
+    newObj.name = employee[i].name
+    console.log( newObj.name );
+  }
+}
+
+// * use each employee object as the input to the function described below.
+// * `console.log` the results of each iteration.
+
+
+
+// ## Function Logic
+
+// Write a declared function that takes in one **Employee** object (as an argument to the function), and returns a new **object** with the following properties:
+
+// * The `name` property should contain the employee's name.
+// * The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+// * The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
+// * The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
+
+     // test person below
+// {
+//   name: 'Atticus',
+//   employeeNumber: '2405',
+//   annualSalary: '47000',
+//   reviewRating: 3
+// },
