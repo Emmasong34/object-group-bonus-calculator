49abaa74c24ff4f814872e5764a1e509b2cb5b87
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-09-30 10:04:23 -0500

    bonusRating function

--- client.js
+++ client.js
@@ -56,14 +56,9 @@ console.log( employees );
 // Loop over the `employees` array and do the following:
 
 for( i = 0; i < employees.length; i++ ){
-  console.log( 'In for loop', employees[i] );
-  function bonusCalculator( employees ){
 
-    console.log('in bonusCalc');
-  }
 }
 
-bonusCalculator(employees);
 // * use each employee object as the input to the function described below.
 // * `console.log` the results of each iteration.
 
@@ -86,3 +81,35 @@ bonusCalculator(employees);
 //   annualSalary: '47000',
 //   reviewRating: 3
 // },
+
+function bonusRating( employees ){
+  if( employees.reviewRating <= 2 ){
+    return 'No bonus';
+  }
+  else if( employees.reviewRating == 3 ){
+    return '4% bonus';
+  }
+  else if( employees.reviewRating == 4 ){
+    return '6% bonus';
+  }
+  else if( employees.reviewRating == 5 ){
+    return '10% bonus';
+  }
+}
+
+console.log( bonusRating( employees[0] ) );
+console.log( bonusRating( employees[1] ) );
+console.log( bonusRating( employees[2] ) );
+console.log( bonusRating( employees[3] ) );
+console.log( bonusRating( employees[4] ) );
+// ### Individual Bonus calculation
+// - Those who have a rating of a 2 or below should not receive a bonus.
+// - Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+// - Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+// - Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+// - If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+// and should receive an additional 5%.
+// - However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
+// - No bonus can be above 13% or below 0% total.
+
+// NOTE: You may abstract out this bonus calculation into a second function if you like, but this is not mandatory.
